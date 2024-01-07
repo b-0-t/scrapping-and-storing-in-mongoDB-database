407f7ec1f933adee10b8e2d2547dc727a84319f0
dhruv <dhruvjn110327>
2024-01-07 23:52:07 +0530

    html fetching

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+# install all dependecies 
+
 import requests
 from bs4 import BeautifulSoup as bs
 
 # inspecting indeed.com to get a overview 
+
+
+job="python+developer"
+
+url = "https://in.indeed.com/jobs?q="+job+"&l"
+# print(url)
+response = requests.get(url)
+soup = bs(response.text,'html.parser')
+# print(soup)
+
+
+
+
